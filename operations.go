46a15456773982cb183cf3f297dc892099daa608
matias-yii <matias-yii@outlook.com>
2020-05-04 15:59:23 -0300

    bugfixing

--- operations.go
+++ operations.go
@@ -1,9 +1,10 @@
 package arithmetic
-package arithmetic
+
+//Factorial factorial of n
 func Factorial(n int) int {
-    var f int = 1
-    for i := 2; i <= n; i++ {
-        f *= i
-    }
-    return f
+	var f int = 1
+	for i := 2; i <= n; i++ {
+		f *= i
+	}
+	return f
 }
